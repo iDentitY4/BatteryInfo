91bef2f6b8100a4e2e1e90c872f978ef46589b2d
Rize <40369614+RizeCrime@users.noreply.github.com>
2019-09-09 17:22:16 +0200

    v1.0
    taskbar icon dynamically hides, app will properly run in background

--- BatteryInfo/BatteryInfo/Form1.cs
+++ BatteryInfo/BatteryInfo/Form1.cs
@@ -17,36 +17,49 @@ namespace BatteryInfo
             InitializeComponent();
 
             // notify icon 
-            batInfoIcon.Visible = true;
+            batInfoIcon.Visible = true; /// make notify icon visible 
+            MenuItem trayItem = new System.Windows.Forms.MenuItem("Exit", trayItem_Click); /// new tray item object with (text, on click event handler)
+            ContextMenu trayMenu = new System.Windows.Forms.ContextMenu(); /// create context menu object 
+            trayMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] { trayItem }); /// add list of items to context menu (currently only one)
+            batInfoIcon.ContextMenu = trayMenu; /// assign created context menu to notify icon 
+            
             
 
             // create 1s timer to update gui 
-            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-            timer.Interval = (1000);
-            timer.Tick += new EventHandler(updateWindow);
-            timer.Start();
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer(); /// create new !Forms! timer object (do NOT use Thread.Timer)
+            timer.Interval = (1000); /// set interval for 1s 
+            timer.Tick += new EventHandler(updateWindow); /// add event handler on tick
+            timer.Start(); 
 
         }
 
-        private void updateWindow(object sender, EventArgs e)
+        
+
+        private void updateWindow(object sender, EventArgs e) /// auto update gui on every timer call 
         {
+            // hide taskbar icon if minimized (keep notify icon)
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide(); /// hide taskbar icon if window is minimized 
+            }
+
             // battery percent (batChargePercent & batChargeLabel)
-            float batPercent = SystemInformation.PowerStatus.BatteryLifePercent * 100;
-            batChargePercent.Text = ((int)batPercent).ToString() + '%';
-            batChargeBar.Value = (int)batPercent;
+            float batPercent = SystemInformation.PowerStatus.BatteryLifePercent * 100; /// battery percent gets returned as decimal of 1
+            batChargePercent.Text = ((int)batPercent).ToString() + '%'; 
+            batChargeBar.Value = (int)batPercent; /// assign batPercent value to progress bar 
 
             // log full charge 
-            if ((int)batPercent >= 90)
+            if ((int)batPercent >= 90) 
             {
-                System.IO.File.WriteAllText(@"C:\Users\super\Code\C#\BatteryInfo\log.txt", DateTime.Now.ToString());
+                System.IO.File.WriteAllText(@"C:\Users\super\Code\C#\BatteryInfo\log.txt", DateTime.Now.ToString()); /// write current time to file if charge >90%
             }
             // compare full charge to now 
-            DateTime fullCharge = Convert.ToDateTime(System.IO.File.ReadAllText(@"C:\Users\super\Code\C#\BatteryInfo\log.txt"));
-            TimeSpan sinceCharge = DateTime.Now.Subtract(fullCharge);
+            DateTime fullCharge = Convert.ToDateTime(System.IO.File.ReadAllText(@"C:\Users\super\Code\C#\BatteryInfo\log.txt")); /// get time of last full charge
+            TimeSpan sinceCharge = DateTime.Now.Subtract(fullCharge); /// compare to current time for "time since last charge"
 
             // charging status (batChargingStatusInfo)
-            PowerLineStatus chargingStatus = SystemInformation.PowerStatus.PowerLineStatus;
-            if (chargingStatus.ToString() == "Online")
+            PowerLineStatus chargingStatus = SystemInformation.PowerStatus.PowerLineStatus; /// get powerline status 
+            if (chargingStatus.ToString() == "Online") /// if else statement is only useful to change the string that gets displayed 
             {
                 string batChargingState = "Charging";
                 // set gui text
@@ -68,15 +81,24 @@ namespace BatteryInfo
                 + "\n" + "Time since last charge: " + Math.Round(sinceCharge.TotalHours, 2) + 'h';
             }
 
-            timeSinceLastFullInfo.Text = Math.Round(sinceCharge.TotalHours, 2).ToString() + 'h';
+            timeSinceLastFullInfo.Text = Math.Round(sinceCharge.TotalHours, 2).ToString() + 'h'; /// put time since last charge on screen 
+
+            ///// to-do:
+            ///     run on startup and in notification area only
 
             
 
         }
 
-        private void BatInfoIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        private void trayItem_Click(object sender, EventArgs e) /// on clicking "Exit", the app will ... exit... who would've thought
         {
+            Application.Exit();
+        }
 
+        private void BatInfoIcon_MouseDoubleClick(object sender, MouseEventArgs e) /// on double clicking the icon, app will be put back to foreground 
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
         }
     }
 }
